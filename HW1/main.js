9670120a4c1cdfb8ad42c569b374ae0fc24da95c
IhorIhnatenko <soljrsoljrsoljr@gmail.com>
2025-04-17 22:03:57 +0300

    Add files via upload

---
+++ HW1/main.js
@@ -0,0 +1,5 @@
+let  userAgeValue = 55;
+let userNameNickname = "Don Pedro Huan Carlo Julio Garsia de la Vega";
+
+userAgeValue = 56;
+userNameNickname = "Don Pedro Huan Carlo Julio Garsia de la Vega II";
