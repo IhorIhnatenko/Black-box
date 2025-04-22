8a8433ff528427868284e738c59658719e42b8a6
IhorIhnatenko <soljrsoljrsoljr@gmail.com>
2025-04-22 16:46:41 +0300

    Add files via upload

---
+++ HW-2/main.js
@@ -0,0 +1,24 @@
+// Операції з запропонованими прикладами
+console.log("Hello, world!");
+console.log(`(5 + 3) * 2 - 7 = ${((5 + 3) * 2 - 7)}`);
+console.log(`(10 / 2) ** 2 = ${((10 / 2) ** 2)}`);
+console.log(`2 * (4 + 3) - (8 / 2) = ${((2 * (4 + 3) - (8 / 2)))}`);
+console.log(`(15 % 4) + 5 = ${((15 % 4) + 5)}`);
+console.log(`3 - (2 ** 4) = ${((3 - (2 ** 4)))}`);
+
+// Операції із вигаданими змінними
+let OwnMoney = 850;
+let CreditMoney = 150;
+
+// - Віднімання
+console.log(`Віднімання: ${OwnMoney - CreditMoney}`);
+// + Додавання
+console.log(`Додавання: ${OwnMoney + CreditMoney}`);
+// * Множення
+console.log(`Множення: ${OwnMoney * CreditMoney}`);
+// / Ділення
+console.log(`Ділення: ${OwnMoney / CreditMoney}`);
+// % Залишок від ділення
+console.log(`Залишок від ділення: ${OwnMoney % CreditMoney}`);
+// ** піднесення до степеня
+console.log(`Піднесення до степеня: ${OwnMoney ** CreditMoney}`);
